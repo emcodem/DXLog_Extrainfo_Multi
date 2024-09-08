be8281751c3b16b2e9f8e3a3750ae8c630aaa68d
emcodem <emcodem@ffastrans.com>
2024-09-08 15:59:09 +0200

    initial

---
+++ DXLCusForm1/CusFrmTest1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DXLog.net
+{
+    partial class ExtraInfoMulti
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxEdiLogFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBrowseEdiLogs = new System.Windows.Forms.Button();
+            this.labelEdiEntryCount = new System.Windows.Forms.Label();
+            this.dataGridViewFoundCalls = new System.Windows.Forms.DataGridView();
+            this.buttonPasteEdiLogs = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFoundCalls)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxEdiLogFolder
+            // 
+            this.textBoxEdiLogFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEdiLogFolder.Location = new System.Drawing.Point(135, 15);
+            this.textBoxEdiLogFolder.Name = "textBoxEdiLogFolder";
+            this.textBoxEdiLogFolder.Size = new System.Drawing.Size(407, 26);
+            this.textBoxEdiLogFolder.TabIndex = 2;
+            this.textBoxEdiLogFolder.TextChanged += new System.EventHandler(this.textBoxEdiLogFolder_TextChanged);
+            this.textBoxEdiLogFolder.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxEdiLogFolder_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "EDI Log Folder: ";
+            // 
+            // buttonBrowseEdiLogs
+            // 
+            this.buttonBrowseEdiLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowseEdiLogs.Location = new System.Drawing.Point(548, 6);
+            this.buttonBrowseEdiLogs.Name = "buttonBrowseEdiLogs";
+            this.buttonBrowseEdiLogs.Size = new System.Drawing.Size(75, 42);
+            this.buttonBrowseEdiLogs.TabIndex = 4;
+            this.buttonBrowseEdiLogs.Text = "Browse";
+            this.buttonBrowseEdiLogs.UseVisualStyleBackColor = true;
+            this.buttonBrowseEdiLogs.Click += new System.EventHandler(this.buttonBrowseEdiLogs_Click);
+            this.buttonBrowseEdiLogs.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonBrowseEdiLogs_MouseDown);
+            // 
+            // labelEdiEntryCount
+            // 
+            this.labelEdiEntryCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelEdiEntryCount.AutoSize = true;
+            this.labelEdiEntryCount.Location = new System.Drawing.Point(715, 17);
+            this.labelEdiEntryCount.Name = "labelEdiEntryCount";
+            this.labelEdiEntryCount.Size = new System.Drawing.Size(76, 20);
+            this.labelEdiEntryCount.TabIndex = 5;
+            this.labelEdiEntryCount.Text = "Entries: 0";
+            // 
+            // dataGridViewFoundCalls
+            // 
+            this.dataGridViewFoundCalls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewFoundCalls.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFoundCalls.Location = new System.Drawing.Point(12, 55);
+            this.dataGridViewFoundCalls.Name = "dataGridViewFoundCalls";
+            this.dataGridViewFoundCalls.RowHeadersWidth = 62;
+            this.dataGridViewFoundCalls.RowTemplate.Height = 15;
+            this.dataGridViewFoundCalls.Size = new System.Drawing.Size(814, 128);
+            this.dataGridViewFoundCalls.TabIndex = 6;
+            // 
+            // buttonPasteEdiLogs
+            // 
+            this.buttonPasteEdiLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPasteEdiLogs.Location = new System.Drawing.Point(629, 6);
+            this.buttonPasteEdiLogs.Name = "buttonPasteEdiLogs";
+            this.buttonPasteEdiLogs.Size = new System.Drawing.Size(80, 42);
+            this.buttonPasteEdiLogs.TabIndex = 7;
+            this.buttonPasteEdiLogs.Text = "Paste";
+            this.buttonPasteEdiLogs.UseVisualStyleBackColor = true;
+            this.buttonPasteEdiLogs.Click += new System.EventHandler(this.buttonPasteEdiLogs_Click);
+            this.buttonPasteEdiLogs.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonPasteEdiLogs_MouseDown);
+            this.buttonPasteEdiLogs.MouseHover += new System.EventHandler(this.buttonPasteEdiLogs_MouseHover);
+            // 
+            // ExtraInfoMulti
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 195);
+            this.Controls.Add(this.buttonPasteEdiLogs);
+            this.Controls.Add(this.dataGridViewFoundCalls);
+            this.Controls.Add(this.labelEdiEntryCount);
+            this.Controls.Add(this.buttonBrowseEdiLogs);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEdiLogFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormID = 1000;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ExtraInfoMulti";
+            this.Text = "Extra Info Multi";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFoundCalls)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxEdiLogFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBrowseEdiLogs;
+        private System.Windows.Forms.Label labelEdiEntryCount;
+        private System.Windows.Forms.DataGridView dataGridViewFoundCalls;
+        private System.Windows.Forms.Button buttonPasteEdiLogs;
+    }
+}
---
+++ DXLCusForm1/CusFrmTest1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Forms;
+using DXLog.net;
+
+namespace DXLog.net
+{
+    public partial class ExtraInfoMulti : KForm
+    {
+        public static String CusWinName
+        {
+            get { return "ExtraInfoMulti"; }
+        }
+
+        public static Int32 CusFormID
+        {
+            get { return 20223; }
+        }
+        
+        private ContestData _cdata = null;
+        private Font _windowFont = new Font("Courier New", 10, FontStyle.Regular);
+        private FrmMain mainForm = null;
+
+        private List<EdiEntry>m_allEntries = new List<EdiEntry>();
+
+        public ExtraInfoMulti()
+        {
+            InitializeComponent();
+            
+            dataGridViewFoundCalls.RowTemplate.Height = 15;
+            dataGridViewFoundCalls.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dataGridViewFoundCalls.RowHeadersVisible = false;
+            //dataGridViewFoundCalls.Columns[0].Name = "CALL";
+            //dataGridViewFoundCalls.Columns[0].DataPropertyName = "CALL";
+            //dataGridViewFoundCalls.Columns[1].Name = "LOC";
+            //dataGridViewFoundCalls.Columns[1].DataPropertyName = "LOC";
+
+        }
+
+        public ExtraInfoMulti(ContestData cdata)
+        {
+            InitializeComponent();
+            ColorSetTypes = new String[] { "Background", "Color", "Header back color", "Header color", "Footer back color", "Footer color", "Final score color", "Selection back color", "Selection color" };
+            DefaultColors = new Color[] { Color.Turquoise, Color.Black, Color.Gray, Color.Black, Color.Silver, Color.Black, Color.Blue, Color.SteelBlue, Color.White };
+            _cdata = cdata;
+            this.FormLayoutChangeEvent += new FormLayoutChange(handle_FormLayoutChangeEvent);
+
+        }
+
+        private void handle_FormLayoutChangeEvent()
+        {
+            InitializeLayout();
+        }
+
+        public override void InitializeLayout()
+        {
+            /* attach to callsign input event from DXLOG */
+            base.InitializeLayout(_windowFont);
+            if (base.FormLayout.FontName.Contains("Courier"))
+                _windowFont = new Font(base.FormLayout.FontName, base.FormLayout.FontSize, FontStyle.Regular);
+            else
+                _windowFont = Helper.GetSpecialFont(FontStyle.Regular, base.FormLayout.FontSize);
+
+            if (mainForm == null)
+            {
+                mainForm = (FrmMain)(this.ParentForm == null ? this.Owner : this.ParentForm);
+                if (mainForm != null) { 
+                    mainForm.CallsignInfoRefreshed += callsigninfoRefreshEvent;
+
+                }
+            }
+
+        }
+
+        public void callsigninfoRefreshEvent(string callsign)
+        {
+
+            //List<EdiEntry> result = m_allEntries.FindAll(x => x.CALL.Contains(callsign));
+                lock (dataGridViewFoundCalls) {
+                    if (dataGridViewFoundCalls.InvokeRequired)
+                    {
+                        dataGridViewFoundCalls.Invoke(new MethodInvoker(() => 
+                        dataGridViewFoundCalls.DataSource = m_allEntries.Where(x => x.CALL.Contains(callsign)).ToList().GroupBy(x => x.LOC).Select(x => x.First()).ToList()
+
+                        ));
+                    }
+                    else
+                    {
+                        dataGridViewFoundCalls.DataSource = m_allEntries.Where(x => x.CALL.Contains(callsign)).ToList().GroupBy(x => x.LOC).Select(x => x.First()).ToList();
+
+                    }
+                }
+
+
+            }
+
+
+
+
+        //HELPERS
+
+        private void parseEdiLogs(string root) {
+            DirectoryInfo d = new DirectoryInfo(root); //Assuming Test is your Folder
+
+            FileInfo[] Files = d.GetFiles("*.edi"); //Getting Text files
+            string str = "";
+
+            m_allEntries.Clear();
+
+            foreach (FileInfo file in Files)
+            {
+                m_allEntries.AddRange(ediToCallSignList(file.FullName));
+            }
+            labelEdiEntryCount.Text = "Count: " + m_allEntries.Count.ToString();
+            dataGridViewFoundCalls.DataSource = null;
+            dataGridViewFoundCalls.DataSource = m_allEntries;
+           // (dataGridViewFoundCalls.DataSource as DataTable).DefaultView.RowFilter = string.Format("CALL LIKE %{0}%", "OE1W");
+            //dataGridViewFoundCalls.DataMember = "CALL";
+            //dataGridViewFoundCalls.DataMember = "LOC";
+
+        }
+        public List<EdiEntry> ediToCallSignList(string path)
+        {
+            List<EdiEntry> list = new List<EdiEntry>();
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+                foreach (string line in lines)
+                {
+                    try
+                    {
+                        var split = line.Split(';');
+                        if (split.Length == 15)
+                        {
+                            var callsign = split[2];
+                            var locator = split[9];
+                            list.Add(new EdiEntry(callsign,locator));
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            catch (Exception exc) { }
+
+            return list;
+        }
+
+        private void textBoxEdiLogFolder_TextChanged(object sender, EventArgs e)
+        {
+            try { 
+                parseEdiLogs(textBoxEdiLogFolder.Text);
+            }catch(Exception exc) { }
+        }
+
+        private void textBoxEdiLogFolder_DragEnter(object sender, DragEventArgs e)
+        {
+            //Console.WriteLine("DragEnter!");
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void buttonPasteEdiLogs_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                string clipboardText = Clipboard.GetText(TextDataFormat.Text);
+                textBoxEdiLogFolder.Text = clipboardText;
+                // Do whatever you need to do with clipboardText
+            }
+            else {
+                mainForm.Invoke(new MethodInvoker(() =>
+                    MessageBox.Show("No text in clipboard")
+                ));
+            }
+        }
+
+        private void buttonPasteEdiLogs_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.Show("Paste from Clipboard", buttonPasteEdiLogs, 0, 0, 1000);
+        }
+
+        private void buttonPasteEdiLogs_MouseDown(object sender, MouseEventArgs e)
+        {
+            buttonPasteEdiLogs_Click(null, null);
+        }
+        private void buttonBrowseEdiLogs_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Select Current Folder.";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = Path.GetDirectoryName(folderBrowser.FileName);
+                textBoxEdiLogFolder.Text = folderPath;
+                parseEdiLogs(folderPath);
+            }
+
+        }
+        private void buttonBrowseEdiLogs_MouseDown(object sender, MouseEventArgs e)
+        {
+            buttonBrowseEdiLogs_Click(null,null);
+        }
+    }
+}
+
+public class EdiEntry {
+    private string _CALL;
+    private string _LOC;
+    public string CALL { get { return _CALL; } }
+    public string LOC { get { return _LOC; } }
+    public EdiEntry(string call, string loc)
+    {
+
+
+        _CALL = call;
+        _LOC = loc;
+    }
+
+
+}
